c7ee16dd73988c50768aa5e55b473338a3e9544b
rushabh_khara <rushabhkhara70@gmail.com>
2019-03-02 17:24:59 +0530

    signup page and login page

--- finTech/src/app/app.module.ts
+++ finTech/src/app/app.module.ts
@@ -9,18 +9,26 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { LoginsignupComponent } from './loginsignup/loginsignup.component';
+import { Signup1Component } from './signup1/signup1.component';
+import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginsignupComponent
+    Signup1Component,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+      FormsModule,
+      ReactiveFormsModule,
       AngularFireAuthModule,
       AngularFireDatabaseModule,
       AngularFirestoreModule,
-      AngularFireModule.initializeApp(environment.Firebaseconfig)
+      AngularFireModule.initializeApp(environment.Firebaseconfig),
+      RouterModule.forRoot([
+      {path:'login',component:LoginComponent},
+        {path:'signup1',component:Signup1Component}
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
